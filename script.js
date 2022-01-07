24a9dbfc5726c52660e840788bee6565ffe91f31
oke-yomi <okeoluwayomi9@gmail.com>
2022-01-07 03:55:17 +0100

    portfolio updated

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function () {
+	$("#menu").click(function () {
+		$(this).toggleClass("fa-times");
+		$("header").toggleClass("toggle");
+	});
 
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
-  });
+	$(window).on("scroll load", function () {
+		$("#menu").removeClass("fa-times");
+		$("header").removeClass("toggle");
 
-  $(window).on('scroll load',function(){
+		if ($(window).scrollTop() > 0) {
+			$(".top").show();
+		} else {
+			$(".top").hide();
+		}
+	});
 
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
+	// smooth scrolling
 
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
-    }
+	$('a[href*="#"]').on("click", function (e) {
+		e.preventDefault();
 
-  });
-
-  // smooth scrolling 
-
-  $('a[href*="#"]').on('click',function(e){
-
-    e.preventDefault();
-
-    $('html, body').animate({
-
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      500, 
-      'linear'
-    );
-
-  });
-
+		$("html, body").animate(
+			{
+				scrollTop: $($(this).attr("href")).offset().top,
+			},
+			500,
+			"linear"
+		);
+	});
 });
+
+function sendMail(params) {
+	let tempParams = {
+		name: document.getElementById("name").value,
+		email: document.getElementById("email").value,
+		phone: document.getElementById("phone").value,
+		project_title: document.getElementById("project_title").value,
+		message: document.getElementById("message").value,
+	};
+
+	emailjs.send("service_43qekuk", "template_otesmbg", tempParams).then(
+		function (res) {
+			console.log("SUCCESS!", res.status);
+		},
+		function (error) {
+			console.log("FAILED...", error);
+		}
+	);
+}
